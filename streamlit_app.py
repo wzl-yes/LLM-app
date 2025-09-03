1159ef36906d0a04768771b68868ad545df1e1b3
wzl-yes <51867121+wzl-yes@users.noreply.github.com>
2025-09-03 06:53:37 +0000

    重构 Streamlit 应用程序以支持自定义 LLM 配置

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,21 +3,29 @@ from langchain_openai.chat_models import ChatOpenAI
 
 st.title("🦜🔗 Quickstart App")
 
-openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+
+llm_base_url = st.sidebar.text_input("LLM Base URL", value="https://dashscope.aliyuncs.com/compatible-mode/v1", type="default")
+llm_model = st.sidebar.text_input("LLM Model", value="deepseek-v3.1", type="default")
+llm_api_key = st.sidebar.text_input("LLM API Key", type="password")
 
 
 def generate_response(input_text):
-    model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
-    st.info(model.invoke(input_text))
+    model = ChatOpenAI(
+        temperature=0.7,
+        api_key=llm_api_key,
+        base_url=llm_base_url,  # 可自定义
+        model_name=llm_model             # 可自定义
+    )
+    message = model.invoke(input_text)
+    st.info(message.content)
 
 
 with st.form("my_form"):
     text = st.text_area(
-        "Enter text:",
-        "What are the three key pieces of advice for learning how to code?",
+        "Enter text:"
     )
     submitted = st.form_submit_button("Submit")
-    if not openai_api_key.startswith("sk-"):
-        st.warning("Please enter your OpenAI API key!", icon="⚠")
-    if submitted and openai_api_key.startswith("sk-"):
+    if not llm_api_key.startswith("sk-"):
+        st.warning("Please enter your LLM API key!", icon="⚠")
+    if submitted and llm_api_key.startswith("sk-"):
         generate_response(text)
